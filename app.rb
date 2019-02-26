3b6a7546a863ac14c81b4808f9e6121d98a9ae4d
Christina Williams <christinaxtwilliams@gmail.com>
2019-02-26 19:07:29 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -10,6 +10,14 @@ class App < Sinatra::Base
     (params[:number].to_i ** 2).to_s
   end
   
+  get "/say/:number/:phrase" do
+    list_phrase = ' '
+    params[:number].to_i.times do
+      list_phrase += params[:phrase]
+    end
+    list_phrase
+  end
+
   
 
 end
